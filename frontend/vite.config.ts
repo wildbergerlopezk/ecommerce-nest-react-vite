f41a3423a6554357b1e25a108c62e6d10eb657f2
wildbergerlopezk <businesswildberger@gmail.com>
2025-08-05 01:34:44 -0300

    1 y 34 am

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,14 @@
-// vite.config.ts o vite.config.js
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', 
+  build: {
+    outDir: 'dist', 
+  },
   server: {
-    host: true, // permite accesos externos
-    allowedHosts: ['.ngrok-free.app'], // permite cualquier subdominio ngrok-free.app
-    // También podés poner directamente tu subdominio completo:
-    // allowedHosts: ['65e23a76ba8a.ngrok-free.app']
+    host: true, 
+    allowedHosts: ['.ngrok-free.app'],
   }
 })
